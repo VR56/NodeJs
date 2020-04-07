4fc382ed74bc63ca34599e5ad64dfe7d0b8bb9a9
VR56 <53446898+VR56@users.noreply.github.com>
2020-04-07 15:50:18 +0530

    comitted

--- Assignment1.js
+++ Assignment1.js
@@ -1,7 +1,10 @@
-const path = require("path")
-const fs = require("fs")
+const path = require("path");
+const fs = require("fs");
+const crypto = require('crypto-js');
+
  
 const args = process.argv;
+let flag = 0;
 if(args[2]===undefined)
     console.log("No arguments are given!");
 else{
@@ -15,10 +18,20 @@ else{
                 } else {
                     console.info(filepath); 
                     let data = fs.readFileSync(filepath);
-                    let fileData=filepath+"   "+data+`\n`;
-                    var logger = fs.createWriteStream('log.txt', {
-                        flags: 'a' 
-                      })
+                    let encryptedMD5Data=crypto.MD5(data).toString();
+                    let encryptedSHA1Data=crypto.SHA1(data).toString();
+                    let fileData=filepath+"   "+encryptedMD5Data+"    "+encryptedSHA1Data+`\n`;
+                    if(flag===0){
+                        var logger = fs.createWriteStream('log.txt', {
+                            flags: 'w' 
+                          }) 
+                          flag++;
+                    }
+                    else{
+                        var logger = fs.createWriteStream('log.txt', {
+                            flags: 'a' 
+                          })
+                    }
                     logger.write(fileData)
                 }    
             }); 
